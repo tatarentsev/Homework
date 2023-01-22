ceaa42536e3bc897c82a3c6ee7bec4b5e0299e6c
ilyaTATARENTSEV <kaxaaapbg@mail.ru>
2023-01-22 13:12:49 +0300

    Example №19

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите пятизначное число, а я проверю является ли оно палиндромом: ");
+string number = Console.ReadLine();
+int len = number.Length;
+
+if (len == 5)
+{
+    if (number[0] == number[4] & number[1] == number[3]) 
+    {
+        Console.Write("да");
+    }
+
+    else
+    {
+        Console.Write("нет");
+    }
+}
