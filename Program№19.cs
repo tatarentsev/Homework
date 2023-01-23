58bc0166534ffdb5102381a11e65217f4d625f01
ilyaTATARENTSEV <kaxaaapbg@mail.ru>
2023-01-23 18:58:45 +0300

    Решил без строк)

---
+++ Program№19.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите пятизначное число, а я проверю является ли оно палиндромом: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int n1 = number % 10; // последняя цифра
+int n2 = (number % 100) / 10; // предпоследняя цифра
+int n4 = number / 1000 % 10;
+int n5 = number / 10000;
+
+if (n1 == n5 & n2 == n4)
+{
+    Console.WriteLine($"Число {number} является палиндромом");
+}
+
+else
+{
+    Console.WriteLine($"Число {number} не является палиндромом");
+}
