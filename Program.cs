de6de57d4b3da54df1a7678d7d6658727fe37e71
ilyaTATARENTSEV <kaxaaapbg@mail.ru>
2023-01-24 20:16:41 +0300

    Ex1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите цифру, обозначающую день недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string DayOfWeek(int arg1)
+{
+    string result = "";
+    if (arg1 > 0 & arg1 <= 5) result = "нет";
+    if (arg1 > 5 & arg1 < 8) result = "да";
+    return result;
+}
+
+string day = DayOfWeek(number);
+Console.Write(day); 
